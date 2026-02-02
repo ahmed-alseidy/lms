8058a516d94c034dfe36ceeb0d4455465f089f26
ahmed-alseidy <ahmedalseidy4@gmail.com>
2026-02-02 17:02:22 +0200

    fix(description-form): update initial text value to include a newline character for improved formatting

--- apps/web/app/[subdomain]/dashboard/courses/[courseId]/sections/[sectionId]/lessons/[lessonId]/_components/description-form.tsx
+++ apps/web/app/[subdomain]/dashboard/courses/[courseId]/sections/[sectionId]/lessons/[lessonId]/_components/description-form.tsx
@@ -46,7 +46,7 @@ const initialValue = {
             format: 0,
             mode: "normal",
             style: "",
-            text: "",
+            text: "\n",
             type: "text",
             version: 1,
           },
